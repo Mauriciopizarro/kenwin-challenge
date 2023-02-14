cad3660df7a4dcd764894b7beeb41e1a9a29c9a1
Mauricio Pizarro <pizarromauricio031096@gmail.com>
2023-02-13 21:52:53 -0300

    Add views, improve controllers and readme (#12)

--- starter.py
+++ starter.py
@@ -1,12 +1,11 @@
 import infrastructure.Injector # no remove this dependecy
-from config import settings
 from infrastructure.controllers import \
     RegisterUserController, \
     AuthController, \
     ResetPasswordController, \
     CreateTaskController, \
-    GetTaskController, FinishTaskController
-from fastapi.middleware.cors import CORSMiddleware
+    GetTaskController, \
+    FinishTaskController
 from fastapi import FastAPI
 import logging.config
 import yaml
@@ -19,18 +18,8 @@ logging.config.dictConfig(config)
 logger = logging.getLogger(__name__)
 logger.info("Configured the logger!")
 
-origins = [
-    settings.CLIENT_ORIGIN,
-]
-
 app = FastAPI()
-app.add_middleware(
-    CORSMiddleware,
-    allow_credentials=True,
-    allow_origins=origins,
-    allow_methods=["*"],
-    allow_headers=["*"],
-)
+
 app.include_router(RegisterUserController.router)
 app.include_router(AuthController.router)
 app.include_router(ResetPasswordController.router)
